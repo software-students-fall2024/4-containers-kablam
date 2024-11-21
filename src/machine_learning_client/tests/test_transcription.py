d57edde7af05806eb3d6e56a61d24db15c0ff7fc
seeyeh <will.yn.xie@gmail.com>
2024-11-21 12:19:42 -0500

    Added a few lines to get the correct file path to the test audio file

--- src/machine_learning_client/tests/test_transcription.py
+++ src/machine_learning_client/tests/test_transcription.py
@@ -3,10 +3,16 @@
 # import os
 # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+import os
+# Get the directory where the current script is located
+current_dir = os.path.dirname(__file__)
+# Construct the full path to the audio file
+audio_file_path = os.path.join(current_dir, "normalAudio.wav")
+
 import pytest
 from speech_recog import transcription
 
 def test_transcribe_normal_hello():
-    text = transcription("./normalAudio.wav")
+    text = transcription(audio_file_path)
     print(text);
     assert text == "hello my name is william"

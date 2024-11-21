f55ee4ab7462f5e93ac035c904fe1413c2a41bc7
seeyeh <will.yn.xie@gmail.com>
2024-11-21 13:06:14 -0500

    Pushing the normalAudio wav test file to repo, was not before

--- src/machine_learning_client/tests/test_transcription.py
+++ src/machine_learning_client/tests/test_transcription.py
@@ -1,16 +1,10 @@
-# # For testing purposes, so I can get access to speech_recog testing file
-# import sys
-# import os
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 import os
+import pytest
+from speech_recog import transcription
 # Get the directory where the current script is located
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to the audio file
 audio_file_path = os.path.join(current_dir, "normalAudio.wav")
-
-import pytest
-from speech_recog import transcription
 
 def test_transcribe_normal_hello():
     text = transcription(audio_file_path)

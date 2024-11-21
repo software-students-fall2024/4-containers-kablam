7039427f99121da385ad2c73853816319d7b000b
seeyeh <will.yn.xie@gmail.com>
2024-11-21 12:55:11 -0500

    Another fixing file path attempt

--- src/machine_learning_client/tests/test_transcription.py
+++ src/machine_learning_client/tests/test_transcription.py
@@ -5,7 +5,7 @@
 
 import os
 # Get the directory where the current script is located
-current_dir = os.path.abspath(os.path.dirname(__file__))
+current_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to the audio file
 audio_file_path = os.path.join(current_dir, "normalAudio.wav")
 
@@ -14,5 +14,6 @@ from speech_recog import transcription
 
 def test_transcribe_normal_hello():
     text = transcription(audio_file_path)
+    print(audio_file_path)
     print(text);
     assert text == "hello my name is william"

9041a4182b989336c04ce3c3ce032af4fe9531be
StephenS2021 <sspencerwong28@gmail.com>
2024-11-22 13:23:33 -0500

    setting up server environment for ml

--- src/machine_learning_client/speech_recog.py
+++ src/machine_learning_client/speech_recog.py
@@ -1,4 +1,7 @@
 import speech_recognition as sr
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 def transcription(file_path):
     r = sr.Recognizer()
@@ -14,3 +17,18 @@ def transcription(file_path):
         raise ValueError("Audio cannot be understood")
     except sr.RequestError as e:
         raise RuntimeError(f"API error; {e}")
+
+@app.route('/accept_file', methods=['POST'])
+def accept_audio():
+    if 'audio' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['audio']
+    # TRANSCRIBE FILE
+
+@app.route('/', methods=['GET'])
+def test_server():
+    return('<h1>Working</h1>')    
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
